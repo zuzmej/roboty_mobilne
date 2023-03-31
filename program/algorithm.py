f221a658a0331dae379bde30d1055066d6f69a5f
Zuzanna <zuzaxmejer@gmail.com>
2023-04-01 01:24:50 +0200

    wstepny zarys funkcji find_finish

--- program/algorithm.py
+++ program/algorithm.py
@@ -11,3 +11,12 @@ class algorithm(data,ABC):
     @abstractmethod
     def solve(self):
         pass
+
+    def find_finish(self) -> list:  #metoda do znajdowania współrzędnych środkowego kwadratu
+        finish_position = []
+        for col in range(len(data.maze)-1):
+            for row in range(len(data.maze[col])-1):
+                if data.maze[col][row] & 0b1100 and data.maze[col][row+1] & 0b1001 and data.maze[col+1][row] & 0b0100 and data.maze[col+1][row+1] & 0b0011:   #pierwszy przypadek
+                    pass    #finish position dodaj 4 współrzędne, nie wiem jak to zrobić z tymi listami ehh
+                #if (...)    #drugi przypadek
+                #if (...)    #trzeci przypadek
