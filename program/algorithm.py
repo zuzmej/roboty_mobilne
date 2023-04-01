4bd5722d48000ceca385ca2ccbcedba7a3ee8064
Zuzanna <zuzaxmejer@gmail.com>
2023-04-01 19:38:14 +0200

    implementacja metody find_finish do znajdowania końca ścieżki; dodanie 2 labiryntow do testowania

--- program/algorithm.py
+++ program/algorithm.py
@@ -12,11 +12,19 @@ class algorithm(data,ABC):
     def solve(self):
         pass
 
-    def find_finish(self) -> list:  #metoda do znajdowania współrzędnych środkowego kwadratu
+    def find_finish(self) -> list:  #metoda do znajdowania współrzędnych końca ścieżki
         finish_position = []
-        for col in range(len(data.maze)-1):
-            for row in range(len(data.maze[col])-1):
-                if data.maze[col][row] & 0b1100 and data.maze[col][row+1] & 0b1001 and data.maze[col+1][row] & 0b0100 and data.maze[col+1][row+1] & 0b0011:   #pierwszy przypadek
-                    pass    #finish position dodaj 4 współrzędne, nie wiem jak to zrobić z tymi listami ehh
-                #if (...)    #drugi przypadek
-                #if (...)    #trzeci przypadek
+        for col in range(len(self.maze)-1): #podwójna pętla -- po kolumnach i wierszach (do przedostatnich)
+            for row in range(len(self.maze[col])-1):
+                if not self.maze[col][row] & 0b0011 and not self.maze[col][row+1] & 0b0110 and not self.maze[col+1][row] & 0b1001 and not self.maze[col+1][row+1] & 0b1100:   #znalezienie dużego kwadratu
+                    if not self.maze[col][row] & algorithm.WEST or not self.maze[col][row] & algorithm.SOUTH:   #znalezienie które pole ma wjazd do środka i przypisanie tej wartości
+                        finish_position = [col, row]
+                    elif not self.maze[col][row+1] & algorithm.NORTH or not self.maze[col][row+1] & algorithm.WEST:
+                        finish_position = [col, row+1]
+                    elif not self.maze[col+1][row] & algorithm.SOUTH or not self.maze[col+1][row] & algorithm.EAST:
+                        finish_position = [col+1, row]
+                    elif not self.maze[col+1][row+1] & algorithm.NORTH or not self.maze[col+1][row+1] & algorithm.EAST:
+                        finish_position = [col+1, row+1]
+                else:
+                    pass
+        return finish_position
