8d02739a9309031a96b899162203dc760fe3b20f
Zuzanna <zuzaxmejer@gmail.com>
2023-04-01 23:23:13 +0200

    mala poprawka

--- program/algorithm.py
+++ program/algorithm.py
@@ -17,13 +17,13 @@ class algorithm(data,ABC):
         for col in range(len(self.maze)-1): #podwójna pętla -- po kolumnach i wierszach (do przedostatnich)
             for row in range(len(self.maze[col])-1):
                 if not self.maze[col][row] & 0b0011 and not self.maze[col][row+1] & 0b0110 and not self.maze[col+1][row] & 0b1001 and not self.maze[col+1][row+1] & 0b1100:   #znalezienie dużego kwadratu
-                    if not self.maze[col][row] & algorithm.WEST or not self.maze[col][row] & algorithm.SOUTH:   #znalezienie które pole ma wjazd do środka i przypisanie tej wartości
+                    if not self.maze[col][row] & self.WEST or not self.maze[col][row] & self.SOUTH:   #znalezienie które pole ma wjazd do środka i przypisanie tej wartości
                         finish_position = [col, row]
-                    elif not self.maze[col][row+1] & algorithm.NORTH or not self.maze[col][row+1] & algorithm.WEST:
+                    elif not self.maze[col][row+1] & self.NORTH or not self.maze[col][row+1] & self.WEST:
                         finish_position = [col, row+1]
-                    elif not self.maze[col+1][row] & algorithm.SOUTH or not self.maze[col+1][row] & algorithm.EAST:
+                    elif not self.maze[col+1][row] & self.SOUTH or not self.maze[col+1][row] & self.EAST:
                         finish_position = [col+1, row]
-                    elif not self.maze[col+1][row+1] & algorithm.NORTH or not self.maze[col+1][row+1] & algorithm.EAST:
+                    elif not self.maze[col+1][row+1] & self.NORTH or not self.maze[col+1][row+1] & self.EAST:
                         finish_position = [col+1, row+1]
                 else:
                     pass
