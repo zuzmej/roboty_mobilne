9717ebaa4f9fdc1c6d7018091c32afe3054e1f82
Zuzanna <zuzaxmejer@gmail.com>
2023-05-02 17:57:38 +0200

    commit w razie czego, przed duzymi zmianami

--- program/right_hand.py
+++ program/right_hand.py
@@ -1,58 +1,71 @@
-# algorytm prawej ręki
 from algorithm import algorithm
+from maze_reader import maze_reader #do testów
+
+## @brief Class containing right hand algorithm 
+#
 
 class right_hand(algorithm):
 
+    ## @brief Constructor to initialize maze
+    #
+    # @param maze is a 2d list representing the layout of the walls in the maze from maze_reader
     def __init__(self, maze):
         self.maze = maze #przekazanie labiryntu z maze_reader
+        self.path = [[0 for _ in range(16)] for _ in range(16)] # zainicjalizowanie listy dwuwymiarowej path zerami
+
+
+    def right_hand_algorithm(self, position, end_position):
+        start_north = self.NORTH
+        start_right = self.EAST
+        start_left = self.WEST
+        start_south = self.SOUTH
+        column = position[0]    # obecne położenie
+        row = position[1]
+        self.path[column][row] = 1     # wpisanie 1 do listy tam, gdzie znalazł się robot
+        for i in range(7):
+            if position != end_position:   # dopóki robot nie dotrze do końca ścieżki
+                print("\nZaczynamy\n")
+                if not self.maze[column][row] & self.EAST: #jeśli nie ma prawej ściany
+                    column += 1                                 # "jedzie w prawo" -- zwiększa się kolumna
+                    position = [column, row]
+                    self.path[column][row] = 1     # zaznacza ścieżkę
+                elif not self.maze[column][row] & self.NORTH:  #jest prawa ściana, ale nie ma przedniej
+                    print("Tu nie ma przedniej ściany: ", position)
+                    row += 1                                    # "jedzie do przodu" -- zwiększa się rząd
+                    position = [column, row]
+                    self.path[column][row] = 1
+                elif not self.maze[column][row] & self.WEST:  #są prawa i przednia, ale nie ma lewej ściany
+                    column -= 1                                    # "jedzie w lewo" -- zmniejsza się kolumna
+                    position = [column, row]
+                    self.path[column][row] = 1
+                    print("position 3", position)
+                elif not self.maze[column][row] & self.SOUTH: #są ściany prawa, przednia, lewa, ale nie ma tylnej
+                    row -= 1                                       # "jedzie do tyłu" -- zmniejsza się rząd
+                    position = [column, row]
+                    self.path[column][row] = 1
+                    print("position 4", position)
+                print(position)
+        return self.path
+    
+
 
     def solve(self) -> list:
         column = 0
         row = 0
         position = [column, row]    # pole startowe [0,0]
-        self.path = [[0 for _ in range(16)] for _ in range(16)] # zainicjalizowanie listy dwuwymiarowej path zerami
-        self.path[position[0]][position[1]] = 1     # wpisanie 1 do listy tam, gdzie znalazł się robot
-        while position != self.find_finish():   # dopóki robot nie dotrze do końca ścieżki
-            if not self.maze[position[0]][position[1]] & self.EAST: #jeśli nie ma prawej ściany
-                column += 1                                 # "jedzie w prawo" -- zwiększa się kolumna
-                position = [column, row]
-                self.path[position[0]][position[1]] = 1     # zaznacza ścieżkę
-            elif not self.maze[position[0]][position[1]] & self.NORTH:  #jest prawa ściana, ale nie ma przedniej
-                row += 1                                    # "jedzie do przodu" -- zwiększa się rząd
-                position = [column, row]
-                self.path[position[0]][position[1]] = 1
-            elif not self.maze[position[0]][position[1]] & self.WEST:  #są prawa i przednia, ale nie ma lewej ściany
-                column -= 1                                    # "jedzie w lewo" -- zmniejsza się kolumna
-                position = [column, row]
-                self.path[position[0]][position[1]] = 1
-            elif self.maze[position[0]][position[1]] & self.EAST and self.maze[position[0]][position[1]] & self.NORTH and self.maze[position[0]][position[1]] & self.WEST: #są ściany prawa, przednia, lewa
-                row -= 1                                       # "jedzie do tyłu" -- zmniejsza się rząd
-                position = [column, row]
-                self.path[position[0]][position[1]] = 1
+        end_position = self.find_finish()   #pole końcowe
+        self.right_hand_algorithm(position, end_position)   # wywołanie algorytmu prawej ręki
+        for i in range(15,-1,-1):     # wypisanie wyznaczonej sciezki
+            for j in range(16):
+                print(self.path[j][i], end=" ")
+            print("\n")
+
         return self.path
+        
 
 
 
-# chyba niepotrzebnie robię takie długie elify???
-#dodac ograniczenia, zeby robo nie wyszedl poza labirynt!!!!!!!!!
-# while(robot nie dotrze do konca sciezki){ 
-#     if(nie ma sciany po prawej)             //* 
-#         "ruszam sie w prawo" --- position[kolumna++][wiersz]
-#         zaktualizuj path 
-#     else if(można_jechać_prosto) 
-#         // go
-#     else if(można_skręcić_w_lewo)        //** 
-#         skrec_w_lewo();                  //** 
-#     else 
-#         zawróć(); 
-# jedz_jeden_segment_do_przodu(); 
-# }
 
-tablica_a = [10,10,10]
-tablica_b = [10, 10, 10]
-maze_example = [tablica_a, tablica_b]
-print("maze example: ", maze_example)
-konstr = right_hand(maze_example)
-
-rh = right_hand(maze_example)
+maze = maze_reader()
+rh = right_hand(maze.read_maze("mazes/maze2"))
 rh.solve()
